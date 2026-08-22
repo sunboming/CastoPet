56eabeb81eef5c667847b8b3df0ec259973d4d2d
lemon <2856848002@qq.com>
2026-08-22 21:16:01 +0800

    refactor: consolidate assembly metadata

--- src/CastoPet/Properties/AssemblyInfo.cs
+++ src/CastoPet/Properties/AssemblyInfo.cs
@@ -1,3 +1,7 @@
 using System.Runtime.CompilerServices;
+using System.Windows;
 
 [assembly: InternalsVisibleTo("CastoPet.Tests")]
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None,
+    ResourceDictionaryLocation.SourceAssembly)]
